25e2454652fd2b2bdf5f9dcbac7b3f366bbc3084
KassandraKox <sawiii@mail.ru>
2022-12-23 11:55:25 +0300

    1.1 Найти треугольное число и факториал

---
+++ Task11.java
@@ -0,0 +1,28 @@
+package Seminar;
+
+/*Вычислить n-ое треугольного число(сумма чисел от 1 до n), 
+n! (произведение чисел от 1 до n)*/
+
+import java.util.Scanner;
+
+public class Task11 {
+    public static void main(String[] args) {
+        System.out.print("Введите n: ");
+        Scanner scan = new Scanner(System.in);
+        boolean flag = scan.hasNextInt();
+        if (!flag) {
+            System.out.println("Некорректный ввод");
+            System.exit(0);
+        }
+        int number = scan.nextInt();
+
+        int triangle = 0;
+        int factorial = 1;
+        for (int i = 1; i <= number; i++) {
+            triangle += i;
+            factorial *= i;
+        }
+        System.out.printf("Сумма чисел от 1 до %d равна %d\n", number, triangle);
+        System.out.printf("Произведение чисел от 1 до %d равно %d", number, factorial);
+    }
+}
