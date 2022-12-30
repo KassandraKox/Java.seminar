38dd1b8bd71aa50d00cb240090e92dca7f8751f2
KassandraKox <sawiii@mail.ru>
2022-12-30 14:11:10 +0300

    часть sgl-запроса

---
+++ Task21.java
@@ -0,0 +1,31 @@
+/* Дана строка sql-запроса "select * from students where ". 
+Сформируйте часть WHERE этого запроса, используя StringBuilder. 
+Данные для фильтрации приведены ниже в виде json строки.
+Если значение null, то параметр не должен попадать в запрос.
+Параметры для фильтрации: 
+{"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}*/
+package Seminar;
+
+import java.util.Objects;
+
+
+public class task21 {
+    public static void main(String[] args) {
+        StringBuilder request = new StringBuilder("SELECT * FROM students WHERE ");
+        String filtersString = "{\"name\":\"Ivanov\", \"country\":\"Russia\", \"city\":\"Moscow\", \"age\":\"null\"}";
+        String[] res = filtersString.replace("{\"", "")
+                .replace("\":\"", " ")
+                .replace("\", \"", " ")
+                .replace("\"}", "").split(" ");
+        for (int i = 0; i < res.length; i += 2) {
+            if (i + 2 != res.length && i != 0) {
+                request.append(" AND ");
+            }
+            if (!Objects.equals(res[i + 1], "null")) {
+                request.append(res[i]).append(" = ").append("'").append(res[i + 1]).append("'");
+            }
+        }
+        request.append(";");
+        System.out.println(request);
+    }
+}
